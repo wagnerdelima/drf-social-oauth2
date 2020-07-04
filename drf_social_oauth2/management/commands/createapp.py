5f31b7bdd2239ef6aaf0d21ad296c15d4affa86d
Wagner de Lima <waglds@gmail.com>
2020-07-04 19:17:35 +0200

    refactor: command.py

--- drf_social_oauth2/management/commands/createapp.py
+++ drf_social_oauth2/management/commands/createapp.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+
 from oauth2_provider.models import Application
 from oauth2_provider.generators import generate_client_id, generate_client_secret
 
@@ -9,21 +10,18 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "-ci", "--client_id",
-            help="Client ID (recommeded 40 characters long)"
+            "-ci", "--client_id", help="Client ID (recommeded 40 characters long)"
         )
         parser.add_argument(
-            "-cs", "--client_secret",
-            help="Client Secret (recommeded 128 characters long)"
+            "-cs",
+            "--client_secret",
+            help="Client Secret (recommeded 128 characters long)",
         )
-        parser.add_argument(
-            "-n", "--name",
-            help="Name for the application"
-        )
+        parser.add_argument("-n", "--name", help="Name for the application")
 
     def handle(self, *args, **options):
         new_application = Application(
-            user= User.objects.filter(is_superuser=True)[0],
+            user=User.objects.filter(is_superuser=True)[0],
             client_type="confidential",
             authorization_grant_type="password",
             name=options["name"] or "socialauth_application",
