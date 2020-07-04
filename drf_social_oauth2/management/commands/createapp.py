11845e1fef3a93cfd2b3d44ed7c06ff57ef2b767
Wagner de Lima <waglds@gmail.com>
2020-07-04 17:30:14 +0200

    refactor: rename package and files.

--- drf_social_oauth2/management/commands/createapp.py
+++ drf_social_oauth2/management/commands/createapp.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from oauth2_provider.models import Application
 from oauth2_provider.generators import generate_client_id, generate_client_secret
 
