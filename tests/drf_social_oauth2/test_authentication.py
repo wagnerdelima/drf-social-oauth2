57e15f400a8a1fe152b11eadc59669492ef28a76
Wagner de Lima <wagner.delima@gig.com>
2020-08-07 14:44:08 +0200

    test: first initial tests. testing authentication wrongly formatted token

--- tests/drf_social_oauth2/test_authentication.py
+++ tests/drf_social_oauth2/test_authentication.py
@@ -1,25 +1,43 @@
-from django.conf import settings
-settings.configure()
+import pytest
+from rest_framework.exceptions import AuthenticationFailed
 
 from drf_social_oauth2.authentication import SocialAuthentication
+from django.http.request import HttpRequest
 
 
+def create_request(content: str = 'Bearer'):
+    request = HttpRequest()
+    request.META = {
+        'HTTP_AUTHORIZATION': content
+    }
+    return request
 
-class AuthenticateTextCase:
-    @staticmethod
-    def authorization_header_response():
-        return 'nothing'
 
-    def test_authenticate_no_auth_header_fail(self, monkeypatch):
-        def mock_get(*args, **kwargs):
-            return 'nothing'
+def test_authenticate_no_auth_header_fail():
+    authenticated = SocialAuthentication()
+    assert not authenticated.authenticate(HttpRequest())
 
-        authenticated = SocialAuthentication()
-        # apply the monkeypatch for requests.get to mock_get
-        monkeypatch.setattr(
-            SocialAuthentication,
-            'get_authorization_header',
-            mock_get
-        )
 
-        print(authenticated.authenticate())
+def test_authenticate_no_backend_fail():
+    request = create_request('Bearer')
+    authenticated = SocialAuthentication()
+
+    with pytest.raises(AuthenticationFailed):
+        authenticated.authenticate(request)
+
+
+def test_authenticate_empty_bearer_fail():
+    request = create_request('Bearer facebook')
+    authenticated = SocialAuthentication()
+
+    with pytest.raises(AuthenticationFailed):
+        authenticated.authenticate(request)
+
+
+def test_authenticate_wrongly_formatted_token_fail():
+    token = 'Bearer facebook 401f7ac837da42b9 7f613d789819ff93537bee6a'
+    request = create_request(token)
+    authenticated = SocialAuthentication()
+
+    with pytest.raises(AuthenticationFailed):
+        authenticated.authenticate(request)
