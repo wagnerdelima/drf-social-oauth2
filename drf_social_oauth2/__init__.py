aa2bcfee28f022db0c1dc86378ed3108ce35c122
Wagner de Lima <waglds@gmail.com>
2021-02-21 21:49:02 +0100

    feat: add access token and refresh token as a jwt.

--- drf_social_oauth2/__init__.py
+++ drf_social_oauth2/__init__.py
@@ -1,2 +1,32 @@
 """python-social-auth and oauth2 support for django-rest-framework"""
 __version__ = '1.0.9'
+
+from django.conf import settings
+
+from jose import jwt
+from oauthlib.common import Request
+
+try:
+    from secrets import SystemRandom
+except ImportError:
+    from random import SystemRandom
+
+
+UNICODE_ASCII_CHARACTER_SET = (
+    'abcdefghijklmnopqrstuvwxyz' 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' '0123456789'
+)
+
+
+def generate_token(request: Request, length=30, chars=UNICODE_ASCII_CHARACTER_SET):
+    """Generates a non-guessable OAuth Json Web Token
+    OAuth (1 and 2) does not specify the format of tokens except that they
+    should be strings of random characters. Tokens should not be guessable
+    and entropy when generating the random characters is important. Which is
+    why SystemRandom is used instead of the default random.choice method.
+    """
+    rand = SystemRandom()
+    secret = getattr(settings, 'SECRET_KEY')
+
+    token = ''.join(rand.choice(chars) for x in range(length))
+    jwtted_token = jwt.encode({'token': token}, secret, algorithm='HS256')
+    return jwtted_token
