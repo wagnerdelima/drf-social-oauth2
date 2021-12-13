f25b1bc92c7ed650f365f2a68fb8eaa8c069143e
Manaia Junior <manaiajr.23@gmail.com>
2021-12-13 17:17:59 -0300

    Replace deprecated url with re_path

--- drf_social_oauth2/urls.py
+++ drf_social_oauth2/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.urls import include, re_path
 from oauth2_provider.views import AuthorizationView
 
 from drf_social_oauth2.views import (
@@ -12,13 +12,13 @@ from drf_social_oauth2.views import (
 app_name = 'drfso2'
 
 urlpatterns = [
-    url(r'^authorize/?$', AuthorizationView.as_view(), name='authorize'),
-    url(r'^token/?$', TokenView.as_view(), name='token'),
-    url('', include('social_django.urls', namespace='social')),
-    url(r'^convert-token/?$', ConvertTokenView.as_view(), name='convert_token'),
-    url(r'^revoke-token/?$', RevokeTokenView.as_view(), name='revoke_token'),
-    url(r'^invalidate-sessions/?$', invalidate_sessions, name='invalidate_sessions'),
-    url(
+    re_path(r'^authorize/?$', AuthorizationView.as_view(), name='authorize'),
+    re_path(r'^token/?$', TokenView.as_view(), name='token'),
+    re_path('', include('social_django.urls', namespace='social')),
+    re_path(r'^convert-token/?$', ConvertTokenView.as_view(), name='convert_token'),
+    re_path(r'^revoke-token/?$', RevokeTokenView.as_view(), name='revoke_token'),
+    re_path(r'^invalidate-sessions/?$', invalidate_sessions, name='invalidate_sessions'),
+    re_path(
         r'^disconnect-backend/?$',
         DisconnectBackendView.as_view(),
         name='disconnect_backend',
