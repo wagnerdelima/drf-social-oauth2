7431106093eba1cc07ef89136cfffaec1f334642
Wagner de Lima <wagner_de_lima@epam.com>
2023-04-29 14:03:54 +0200

    refac: ♻️ concatenate strings via f-strings.
    Signed-off-by: Wagner de Lima <wagner_de_lima@epam.com>

--- drf_social_oauth2/backends.py
+++ drf_social_oauth2/backends.py
@@ -17,10 +17,10 @@ class DjangoOAuth2(BaseOAuth2):
 
     name = DRFSO2_PROPRIETARY_BACKEND_NAME
     AUTHORIZATION_URL = reverse(
-        DRFSO2_URL_NAMESPACE + ':authorize' if DRFSO2_URL_NAMESPACE else 'authorize'
+        f'{DRFSO2_URL_NAMESPACE}:authorize' if DRFSO2_URL_NAMESPACE else 'authorize'
     )
     ACCESS_TOKEN_URL = reverse(
-        DRFSO2_URL_NAMESPACE + ':token' if DRFSO2_URL_NAMESPACE else 'token'
+        f'{DRFSO2_URL_NAMESPACE}:token' if DRFSO2_URL_NAMESPACE else 'token'
     )
 
 
