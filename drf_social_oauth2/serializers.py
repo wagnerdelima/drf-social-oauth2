703c4bdd8a0af64233b014e977b5e2fafad0b748
Wagner de Lima <waglds@gmail.com>
2023-06-06 14:10:29 +0200

    feat: ✨ change serializers max_length in fields.
    Signed-off-by: Wagner de Lima <waglds@gmail.com>

--- drf_social_oauth2/serializers.py
+++ drf_social_oauth2/serializers.py
@@ -7,27 +7,27 @@ class InvalidateRefreshTokenSerializer(Serializer):
     Validate the client_id required and length 100 characters for refresh tokens.
     """
 
-    client_id = CharField(max_length=100)
+    client_id = CharField(max_length=200)
 
 
 class InvalidateSessionsSerializer(Serializer):
-    client_id = CharField(max_length=100)
+    client_id = CharField(max_length=200)
 
 
 class ConvertTokenSerializer(Serializer):
-    grant_type = CharField(max_length=32)
-    backend = CharField(max_length=100)
-    client_id = CharField(max_length=100)
-    client_secret = CharField(max_length=255)
-    token = CharField(max_length=500)
+    grant_type = CharField(max_length=50)
+    backend = CharField(max_length=200)
+    client_id = CharField(max_length=200)
+    client_secret = CharField(max_length=500)
+    token = CharField(max_length=2000)
 
 
 class RevokeTokenSerializer(Serializer):
-    client_id = CharField(max_length=100)
-    client_secret = CharField(max_length=255)
-    token = CharField(max_length=500)
+    client_id = CharField(max_length=200)
+    client_secret = CharField(max_length=500)
+    token = CharField(max_length=2000)
 
 
 class DisconnectBackendSerializer(Serializer):
-    backend = CharField(max_length=50)
+    backend = CharField(max_length=200)
     association_id = IntegerField()
